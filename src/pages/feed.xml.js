8ff6f8880b5c9b87173e9420e224e7982f7eccff
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2025-08-01 15:59:01 +0000

    Add a feed

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,27 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import pkg from '../../package.json' with { type: 'json' };
+
+export async function GET(context) {
+  const posts = (await getCollection("post"))
+    .sort((a, b) => {
+      return new Date(b.data.dateCreated) - new Date(a.data.dateCreated);
+    })
+    .slice(0, 10);
+
+  return rss({
+    title: pkg.name
+      .split(/[\W+]/)
+      .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+      .join(" "),
+    description: pkg.description,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+      pubDate: post.data.dateCreated,
+    })),
+    customData: `<language>en-US</language>`,
+  });
+}
